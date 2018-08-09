29f5ba66edf1fc4bfe71643d019b49e2a75b8140
Arnab Das <arnab.das682@gmail.com>
2018-08-10 00:49:15 +0600

    1st day for my project

---
+++ app/src/main/java/com/example/jmidar/mealsystem/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.jmidar.mealsystem;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btn_signin);
+        btnSignUp = findViewById(R.id.btn_signup);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
